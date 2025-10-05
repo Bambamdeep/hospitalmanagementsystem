a7f82dfa3d0186ddc8f9e6fff33256af92a86419
Deepak Kumar <167330665+Bambamdeep@users.noreply.github.com>
2025-10-06 00:01:45 +0530

    Add files via upload

---
+++ hospitalconfigserver/src/test/java/com/hospitalmanagement/configserver/HospitalconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
